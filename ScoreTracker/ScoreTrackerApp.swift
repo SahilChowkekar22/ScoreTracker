181939f464b8e3f09b4079f35679a2a920443a45
Sahil Chowkekar <Sahil.Chowkekar@techconsulting.tech>
2025-06-25 09:17:30 -0400

    Update ScoreTrackerApp.swift

--- ScoreTracker/ScoreTrackerApp.swift
+++ ScoreTracker/ScoreTrackerApp.swift
@@ -5,16 +5,7 @@
 //  Created by Sahil ChowKekar on 6/24/25.
 //
 //
-//import SwiftUI
-//
-//@main
-//struct ScoreTrackerApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//    }
-//}
+
 
 import SwiftUI
 
